8ef9e5f79b51f19f5d28e0aa2dc96bd8c897d23b
Rohit <rohit.ku25112001@gmail.com>
2024-04-16 16:24:12 +0530

    login

--- components/login-signup/login/index.jsx
+++ components/login-signup/login/index.jsx
@@ -1,12 +1,12 @@
-'use client'
+"use client"
 
-import { Button, Form, Input, Divider, Checkbox, message } from 'antd';
-import { LockOutlined, InstagramOutlined, TwitterOutlined, GoogleOutlined } from '@ant-design/icons';
-import Link from 'next/link';
-import { useState } from 'react';
-import axios from 'axios';
-// import jwt from 'jsonwebtoken';
-import { useRouter } from 'next/navigation';
+import { Button, Form, Input, Divider, Checkbox, message } from "antd";
+import { LockOutlined, InstagramOutlined, TwitterOutlined, GoogleOutlined } from "@ant-design/icons";
+import Link from "next/link";
+import { useState } from "react";
+import axios from "axios";
+// import jwt from "jsonwebtoken";
+import { useRouter } from "next/navigation";
 axios.defaults.baseURL = "http://localhost:8080"
 axios.defaults.withCredentials = true;
 
@@ -22,17 +22,17 @@ const Login = () => {
         password:e.password
       });
 
-      localStorage.setItem('user',userData);
+      localStorage.setItem("user",userData);
     }
     else{
-      localStorage.removeItem('user')
+      localStorage.removeItem("user")
     }
 
     delete e.remember;
 
     try{
       // setLoading(true);
-      // const {data} = await axios.get('http://localhost:8080/auth/testing',e,{
+      // const {data} = await axios.get("http://localhost:8080/auth/testing",e,{
       //   headers:{
       //     auth:123
       //   }
@@ -40,24 +40,24 @@ const Login = () => {
       // const encryptedData = jwt.sign(
       //   data,
       //   process.env.NEXT_PUBLIC_SECRET_KEY,
-      //   { expiresIn:'7d' }
+      //   { expiresIn:"7d" }
       //   );
 
-      const {data} = await axios.get('/auth/signin',{
+      const {data} = await axios.get("/auth/signin",{
         params:{
           email:e.email,
           password:e.password
         },
         headers:{
-          'auth':'123'
+          "auth":"123"
         }
       });
 
       // const checkToken = Cookies.get("authToken"); if(!checkToken) 
-      // Cookies.set('authToken',encryptedData,{expires:7});
+      // Cookies.set("authToken",encryptedData,{expires:7});
 
-      router.push('/admin');
-      messageApi.success('login success');
+      router.push("/admin");
+      messageApi.success("login success");
     }
     catch({response}){
       messageApi.error(response.data.message);
@@ -71,36 +71,36 @@ const Login = () => {
     <div className="grid md:grid-cols-2 bg-[#EEF1F6] h-[100vh]">
       {contextHolder}
       <div
-      className='lg:flex flex-col justify-center items-center hidden'
+      className="lg:flex flex-col justify-center items-center hidden"
       style={{
-        backgroundImage:'url(/blob.svg)',
-        backgroundRepeat:'no-repeat',
-        backgroundPosition:'center'
+        backgroundImage:"url(/blob.svg)",
+        backgroundRepeat:"no-repeat",
+        backgroundPosition:"center"
       }}
       >
-        <div className='md:p-12 p-8 text-justify'>
-          <h1 className='text-[30px] text-center font-semibold'>WELCOME TO LOGDY</h1>
-          <p className='py-4 md:px-12'>
+        <div className="md:p-12 p-8 text-justify">
+          <h1 className="text-[30px] text-center font-semibold">WELCOME TO LOGDY</h1>
+          <p className="py-4 md:px-12">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled
           </p>
-          <div className='flex gap-8 justify-center items-center'>
+          <div className="flex gap-8 justify-center items-center">
             <Button type="text" 
-            className='text-[18px] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center'
+            className="text-[18px] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center"
             >
               <GoogleOutlined />
             </Button>
 
             <Button type="text"
-            className='text-[18px] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center'
+            className="text-[18px] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center"
             >
               <TwitterOutlined />
             </Button>
             
             <Button 
             type="text"
-            className='text-[18px] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center'
+            className="text-[18px] text-white rounded-full w-[40px] h-[40px] flex items-center justify-center"
             >
               <InstagramOutlined />
             </Button>
@@ -108,15 +108,15 @@ const Login = () => {
         </div>
       </div>
 
-      <div className='px-4 py-10'>
-        <h1 className='text-center text-[30px] font-semibold py-8'>Sign In</h1>
+      <div className="px-4 py-10">
+        <h1 className="text-center text-[30px] font-semibold py-8">Sign In</h1>
         <Form
           onFinish={handleForm}
           variant="filled"
           layout="vertical"
           initialValues={
             {
-              email:'a@gmail.com',
+              email:"a@gmail.com",
               password:123
             }
           }
@@ -134,7 +134,7 @@ const Login = () => {
               <Input 
                 type="email"
                 placeholder="********"
-               prefix={<LockOutlined className='text-gray-400'/>}
+               prefix={<LockOutlined className="text-gray-400"/>}
               />
             </Form.Item>
 
@@ -150,7 +150,7 @@ const Login = () => {
               <Input 
                type="password" 
                placeholder="********"
-               prefix={<LockOutlined className='text-gray-400'/>}
+               prefix={<LockOutlined className="text-gray-400"/>}
                />
             </Form.Item>
 
@@ -166,12 +166,12 @@ const Login = () => {
             <Form.Item>
                 <Button 
                 type="primary" 
-                htmlType='submit'
+                htmlType="submit"
                 loading={loading}
-                className='w-full bg-yellow-300 py-2 h-auto font-semibold rounded-lg'
+                className="w-full bg-yellow-300 py-2 h-auto font-semibold rounded-lg"
                 >
                 {
-                    loading ? 'Loading' :'Submit'
+                    loading ? "Loading" :"Submit"
                 }
                 </Button>
           </Form.Item>
@@ -181,26 +181,26 @@ const Login = () => {
           <Divider orientation="center">or</Divider>
         </div>
 
-          <div className='text-center pb-4'>
-            <span>Not have acocunt? <Link href="/" className='text-blue-400'>Create</Link></span>
+          <div className="text-center pb-4">
+            <span>Not have acocunt? <Link href="/" className="text-blue-400">Create</Link></span>
           </div>
         
-          <div className='md:hidden flex gap-8 justify-center items-center py-2 '>
+          <div className="md:hidden flex gap-8 justify-center items-center py-2 ">
             <Button type="text" 
-            className='text-[18px] text-gray-500 rounded-full w-[40px] h-[40px] flex items-center justify-center'
+            className="text-[18px] text-gray-500 rounded-full w-[40px] h-[40px] flex items-center justify-center"
             >
               <GoogleOutlined />
             </Button>
 
             <Button type="text"
-            className='text-[18px] text-gray-500 rounded-full w-[40px] h-[40px] flex items-center justify-center'
+            className="text-[18px] text-gray-500 rounded-full w-[40px] h-[40px] flex items-center justify-center"
             >
               <TwitterOutlined />
             </Button>
             
             <Button 
             type="text"
-            className='text-[18px] text-gray-500 rounded-full w-[40px] h-[40px] flex items-center justify-center'
+            className="text-[18px] text-gray-500 rounded-full w-[40px] h-[40px] flex items-center justify-center"
             >
               <InstagramOutlined />
             </Button>
