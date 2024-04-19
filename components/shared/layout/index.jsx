3e0252ecd1bc1756174b167b18aba47b61500522
Rohit <rohit.ku25112001@gmail.com>
2024-04-20 03:35:07 +0530

    new navbar

--- components/shared/layout/index.jsx
+++ components/shared/layout/index.jsx
@@ -139,12 +139,12 @@ const Layout =() =>{
   // };
   
 
-  // const handleMouseEnter =(e) =>{
-  //   setactiveMenu(e)
-  // }
-  // const handleMouseLeave =() =>{
-  //   setactiveMenu(null)
-  // }
+  const handleMouseEnter =(e) =>{
+    setactiveMenu(e)
+  }
+  const handleMouseLeave =() =>{
+    setactiveMenu(null)
+  }
   
     return(
         <>
@@ -218,9 +218,9 @@ const Layout =() =>{
               </div>
             </nav>
 
-            {/* <nav className="bg-[#252525] lg:block hidden">
+            <nav className="bg-[#252525] lg:block hidden">
               <ul className="flex justify-evenly py-3 uppercase font-semibold px-4" onMouseLeave={handleMouseLeave}>
-                <li 
+                {/* <li 
                   className="text-white cursor-pointer text-[13px] relative"
                   onMouseEnter={() => handleMouseEnter('new product')}
                 >
@@ -265,7 +265,7 @@ const Layout =() =>{
                       </div>
                     </div>
                   }
-                </li>
+                </li> */}
                 <li 
                   className="text-white hover:text-gray-300 cursor-pointer text-[13px] relative"
                   onMouseEnter={() => handleMouseEnter('specials')}
@@ -348,7 +348,7 @@ const Layout =() =>{
                   </Link>
                 </li>
              </ul>
-            </nav> */}
+            </nav>
         </>
     )
 }
